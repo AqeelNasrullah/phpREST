ebdc6339caa7f055dbc05ed742214d908c7fa585
Aqeel Nasrullah <aqeelnasrullah1997@gmail.com>
2022-09-27 16:04:15 +0500

    PHP Database connection, Read API

---
+++ api/read.php
@@ -0,0 +1,27 @@
+<?php
+
+header('Access-Control-Allow-Origin: *');
+header('Content-Type: application/json');
+
+require_once '../config/database.php';
+
+$conn = getDatabaseConnection();
+
+
+$query = 'SELECT * FROM users';
+
+$result = $conn->prepare($query);
+
+$result->execute();
+
+$num = $result->rowCount();
+
+if ($num > 0) {
+    $users = array();
+    while ($row = $result->fetch(PDO::FETCH_ASSOC)) {
+        array_push($users, $row);
+    }
+    echo json_encode(array('status' => true, 'data' => $users));
+} else {
+    echo json_encode(array('status' => false, 'message' => 'No user found in database.'));
+}
